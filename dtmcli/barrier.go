1e14b2de725d1c09691ab0372e3a6cb3472b8b73
wjia <wjia@microsoft.com>
2021-09-22 15:56:18 +0800

    minor changes

--- dtmcli/barrier.go
+++ dtmcli/barrier.go
@@ -42,6 +42,7 @@ func BarrierFrom(transType, gid, branchID, branchType string) (*BranchBarrier, e
 }
 
 func insertBarrier(tx Tx, transType string, gid string, branchID string, branchType string, barrierID string, reason string) (int64, error) {
+	// 忽略正常语义的动作时要补插入的记录
 	if branchType == "" {
 		return 0, nil
 	}
@@ -70,25 +71,33 @@ func (bb *BranchBarrier) Call(tx Tx, busiCall BusiFunc) (rerr error) {
 	}()
 	ti := bb
 	originType := map[string]string{
-		BranchCancel:     BranchTry,
-		BranchCompensate: BranchAction,
+		BranchCancel:     BranchTry,    // 用于TCC模式
+		BranchCompensate: BranchAction, // 用于SAGA模式
 	}[ti.BranchType]
+	// 如果是正常语义的动作，originType=""（空字符串），不会插入动作记录（所以affected=0）
+	// 如果是撤销语义的动作，originType=“正常语义的动作”，补插入一条正常语义动作的记录
 	originAffected, _ := insertBarrier(tx, ti.TransType, ti.Gid, ti.BranchID, originType, bid, ti.BranchType)
+	// 如果是正常语义的动作，直接插入
+	// 如果是撤销语义的动作，插入撤销语义动作对应的记录
 	currentAffected, rerr := insertBarrier(tx, ti.TransType, ti.Gid, ti.BranchID, ti.BranchType, bid, ti.BranchType)
 	Logf("originAffected: %d currentAffected: %d", originAffected, currentAffected)
+	// 当前是撤销语义的动作，但是正常语义的动作未执行（补插入成功），说明是UNDO先于DO，属于空补偿，直接返回成功
 	if (ti.BranchType == BranchCancel || ti.BranchType == BranchCompensate) && originAffected > 0 { // 这个是空补偿，返回成功
 		return
 	} else if currentAffected == 0 { // 插入不成功
 		var result sql.NullString
 		err := DBQueryRow(tx, "select 1 from dtm_barrier.barrier where trans_type=? and gid=? and branch_id=? and branch_type=? and barrier_id=? and reason=?",
 			ti.TransType, ti.Gid, ti.BranchID, ti.BranchType, bid, ti.BranchType).Scan(&result)
+		// 当前是正常语义的动作，但是已经有另一个原因（撤销语义）插入的数据，说明DO晚于UNDO，属于悬挂请求，返回失败
 		if err == sql.ErrNoRows { // 不是当前分支插入的，那么是cancel插入的，因此是悬挂操作，返回失败，AP收到这个返回，会尽快回滚
 			rerr = ErrFailure
 			return
 		}
+		// 已经有完全相同的动作（gid，bid，actionid，reason）执行过，根据幂等性，直接返回
 		rerr = err //幂等和空补偿，直接返回
 		return
 	}
+	// 通过barrier，执行动作逻辑
 	rerr = busiCall(tx)
 	return
 }
--- dtmcli/barrier.mysql.sql
+++ dtmcli/barrier.mysql.sql
@@ -3,10 +3,10 @@ create database if not exists dtm_barrier /*!40100 DEFAULT CHARACTER SET utf8mb4
 drop table if exists dtm_barrier.barrier;
 create table if not exists dtm_barrier.barrier(
   id int(11) PRIMARY KEY AUTO_INCREMENT,
-  trans_type varchar(45) default '' ,
-  gid varchar(128) default'',
-  branch_id varchar(128) default '',
-  branch_type varchar(45) default '',
+  trans_type varchar(45) default '' COMMENT '事务类型: saga | xa | tcc | msg',
+  gid varchar(128) default'' COMMENT '事务全局id',
+  branch_id varchar(128) default '' COMMENT '事务分支名称',
+  branch_type varchar(45) default '' COMMENT '事务分支类型 saga_action | saga_compensate | xa',
   barrier_id varchar(45) default '',
   reason varchar(45) default '' comment 'the branch type who insert this record',
   create_time datetime DEFAULT now(),
